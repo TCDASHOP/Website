fb7411689c396b2ce8a1cbc151b0535cd3069265
TCDASHOP <taiki.color.digital.art@gmail.com>
2025-12-21 04:21:08 +0900

    Update main.js

--- assets/ja/main.js
+++ assets/ja/main.js
@@ -1,1 +1,8 @@
+// Disable right-click
+document.addEventListener("contextmenu", (e) => e.preventDefault());
 
+// Disable drag on images
+document.addEventListener("dragstart", (e) => {
+  const t = e.target;
+  if (t && t.tagName === "IMG") e.preventDefault();
+});
